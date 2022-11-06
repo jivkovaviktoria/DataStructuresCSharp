c92d01433caa80a86d69c839d237f32f8f0c78d1
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-11-06 15:06:56 +0200

    Solved "Singly linked list" from Linear data structures - lab

--- LinearDataStructures - lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ LinearDataStructures - lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -6,43 +6,117 @@
 
     public class SinglyLinkedList<T> : IAbstractLinkedList<T>
     {
-        private Node<T> _head;
+        private class Node
+        {
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+        }
+        private Node head;
 
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var node = this.head;
+            this.head = new Node(item, node);
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                var node = this.head;
+                while (node.Next != null)
+                {
+                    node = node.Next;
+                }
+
+                node.Next = newNode;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            if (this.head == null) throw new InvalidOperationException();
+            return this.head.Element;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            if (this.head == null) throw new InvalidOperationException();
+
+            var node = this.head;
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            return node.Element;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            if (this.head == null) throw new InvalidOperationException();
+
+            var oldHead = this.head;
+            this.head = oldHead.Next;
+
+            this.Count--;
+            return oldHead.Element;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            if (this.head == null) throw new InvalidOperationException();
+            if (this.head.Next == null)
+            {
+                var x = this.head;
+                this.head = null;
+                this.Count--;
+                return x.Element;
+            }
+            
+            var node = this.head;
+
+            while (node.Next.Next != null)
+            {
+                node = node.Next;
+            }
+
+            var nodeToRemove = node.Next;
+            node.Next = null;
+
+            this.Count--;
+            return nodeToRemove.Element;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.head;
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
