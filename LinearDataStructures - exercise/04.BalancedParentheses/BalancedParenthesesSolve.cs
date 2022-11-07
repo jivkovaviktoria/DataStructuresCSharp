4235422043717f0595a5c484cc297e4329881abb
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-11-07 16:02:41 +0200

    Solved "Balanced parentheses" from Linear data structures - exercise

--- LinearDataStructures - exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ LinearDataStructures - exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,34 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            var openers = new List<char> { '[', '{', '(' };
+            var closers = new List<char> { ']', '}', ')' };
+            var matchingClosers = new Dictionary<char, char>()
+            {
+                ['['] = ']',
+                ['{'] = '}',
+                ['('] = ')',
+            };
+            var OpendedBrackets = new Stack<char>();
+
+            foreach (char character in parentheses)
+            {
+                if (openers.Contains(character)) OpendedBrackets.Push(character);
+                else 
+                {
+                    if (OpendedBrackets.Count == 0)
+                        return false;
+
+                    if (closers.Contains(character))
+                    {
+                        if (matchingClosers[OpendedBrackets.Peek()] == character) OpendedBrackets.Pop();
+                        else return false;
+                    }
+                }
+            }
+            
+            return true;
         }
     }
 }
+
